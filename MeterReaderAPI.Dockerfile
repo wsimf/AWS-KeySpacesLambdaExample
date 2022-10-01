d8f5476567c5e300cb7d84f891b3260187206ff8
Sudara Fernando <sudara@yahoo.com>
2022-10-02 11:22:57 +1300

    API changes for lambda functions

--- MeterReaderAPI.Dockerfile
+++ MeterReaderAPI.Dockerfile
@@ -1,23 +1,25 @@
-﻿FROM public.ecr.aws/lambda/dotnet:6 AS base
-WORKDIR /app
-
-EXPOSE 80
-EXPOSE 443
-
-FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
+﻿FROM mcr.microsoft.com/dotnet/sdk:6.0 AS build
 WORKDIR /src
+
 COPY ["MeterReadingAPI/MeterReadingAPI.csproj", "MeterReadingAPI/"]
 RUN dotnet restore "MeterReadingAPI/MeterReadingAPI.csproj"  /property:Configuration=Release -v d
+
 COPY . .
 
 WORKDIR "/src/MeterReadingAPI"
 RUN dotnet build "MeterReadingAPI.csproj" -c Release -o /app/build -v d 
 
 FROM build AS publish
-RUN dotnet publish "MeterReadingAPI.csproj" -c Release -o /app/publish -v d 
+RUN dotnet publish "MeterReadingAPI.csproj" \
+    --configuration Release \ 
+    --runtime linux-x64 \
+    --self-contained false \ 
+    --output /app/publish \
+    -p:PublishReadyToRun=true  
 
-FROM base AS final
-WORKDIR /app
+FROM public.ecr.aws/lambda/dotnet:6 AS final
+
+WORKDIR /var/task
 COPY --from=publish /app/publish .
 
-ENTRYPOINT ["dotnet", "MeterReading.Web.API.dll"]
+CMD [ "MeterReading.Web.API::MeterReading.Web.API.Function::FunctionHandler" ]
---
+++ MeterReadingAPI/Function.cs
@@ -0,0 +1,82 @@
+using System.Net;
+using System.Text.Json;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.Core;
+using JetBrains.Annotations;
+using MeterReading.Core.Infrastructure;
+using MeterReading.Core.Options;
+using MeterReading.Core.Services;
+using MeterReading.Core.Services.Default;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace MeterReading.Web.API;
+
+[UsedImplicitly]
+public class Function
+{
+    private readonly ServiceProvider _serviceProvider;
+
+    public Function()
+    {
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json")
+            .AddEnvironmentVariables() // Cassandra options are injected through env variables when running in lambda
+            .Build();
+
+        var services = new ServiceCollection();
+
+        services.Configure<CassandraOptions>(configuration.GetSection(CassandraOptions.SectionName));
+
+        services.AddSingleton<CassandraContext>();
+        services.AddScoped<IMeterReadingService, DefaultMeterReadingService>();
+
+        _serviceProvider = services.BuildServiceProvider();
+
+        LambdaLogger.Log("New Function Context initialised");
+    }
+
+    [UsedImplicitly]
+    public async Task<APIGatewayHttpApiV2ProxyResponse> FunctionHandler(APIGatewayHttpApiV2ProxyRequest @event, ILambdaContext context)
+    {
+        static APIGatewayHttpApiV2ProxyResponse GenerateError(string error) => new()
+        {
+            StatusCode = (int)HttpStatusCode.BadRequest,
+            Body = JsonSerializer.Serialize(new { error }, new JsonSerializerOptions { WriteIndented = true })
+        };
+
+        context.Logger.LogInformation($"Handing HTTP event: {context.AwsRequestId}");
+
+        if (@event.QueryStringParameters?.TryGetValue("date", out string? date) != true || string.IsNullOrWhiteSpace(date))
+        {
+            return GenerateError("Date parameter is required. Pass date as a query parameter with key date");
+        }
+
+        if (@event.QueryStringParameters?.TryGetValue("meterId", out string? meterId) != true || string.IsNullOrWhiteSpace(meterId))
+        {
+            return GenerateError("MeterId parameter is required. Pass mater id as a query parameter with key meterId");
+        }
+
+        if (!DateOnly.TryParseExact(date, "dd-MM-yyyy", out DateOnly parsedDate))
+        {
+            return GenerateError($"Invalid date {date}. Use format dd-MM-yyyy");
+        }
+
+        context.Logger.LogInformation("HTTP content validated successfully");
+
+        using IServiceScope scope = _serviceProvider.CreateScope();
+        var service = scope.ServiceProvider.GetRequiredService<IMeterReadingService>();
+
+        int sum = await service.CalculateSum(meterId, parsedDate).ConfigureAwait(false);
+        var result = new { meterId, date = parsedDate.ToString("dd-MM-yyyy"), sum };
+
+        return new APIGatewayHttpApiV2ProxyResponse
+        {
+            StatusCode = (int)HttpStatusCode.OK,
+            Body = JsonSerializer.Serialize(result, new JsonSerializerOptions { WriteIndented = true })
+        };
+    }
+}
